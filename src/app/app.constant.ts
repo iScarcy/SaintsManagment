796265aa9ea35d4147870e82c23d84f94981416e
iScarcy <56608800+iScarcy@users.noreply.github.com>
2025-01-17 09:16:37 +0100

    aggiornamento baseApiUrl

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -1,2 +1,2 @@
-export const baseApiUrl:string = "http://localhost:5071/api";
-export const baseApiUrlSaints:string = "http://localhost:5071/api/saint";
+export const baseApiUrl:string = "http://192.168.1.14:5071/api";
+export const baseApiUrlSaints:string = "http://192.168.1.14:5071/api/saint";
