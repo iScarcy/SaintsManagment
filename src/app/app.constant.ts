90b6a8ce2160c45e0268b4f9b078690dc1bbe063
iScarcy <56608800+iScarcy@users.noreply.github.com>
2025-01-21 17:07:58 +0100

    aggiornamento baseUrl

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -1,3 +1,3 @@
 export const baseApiUrl:string = "http://192.168.1.83:5071/api";
 export const baseApiUrlSaints:string = "http://192.168.1.83:5071/api/saint";
-export const baseHref = "/saintsmanagements/";
+export const baseHref = "/saintsmanagments/";
